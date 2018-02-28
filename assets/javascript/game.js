253656a5b23a13f3dfd469ebb46a8b7afe2bb1b6
Aaron Moskowitz <macPRO@Aarons-MacBook-Pro-2.local>
2018-02-28 11:27:53 -0500

    mostly working but the counter keeps some value from previous game, weird bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {  //wait till document loads before running
-    var win = 0;
+    var win = 0;    //win and loss are global values that continually update
     var loss = 0;
+    var counter = 0;    //initialize counter
+    values();           //call functions
+    game();
+    //setValues for targetNumber and colorGems
 
-        var targetNumber = Math.floor((Math.random() * 120) + 19);
-        $("#number-to-guess").text(targetNumber);
-        console.log(targetNumber);
-    
+    function values() {
+        targetNumber = Math.floor((Math.random() * 120) + 19);
+
+        console.log(targetNumber)
+        $("#number-to-guess").html("Your Target Number is " + targetNumber);
 
         function randomNum() {
             return (Math.floor((Math.random() * 12) + 1));
@@ -15,9 +20,8 @@ $(document).ready(function () {  //wait till document loads before running
         $("#blueGem").attr("data-num", randomNum());
         $("#greenGem").attr("data-num", randomNum());
         $("#yellowGem").attr("data-num", randomNum());
-
+    }
     // console.log($("#redGem").attr("data-num"));//this WORKS! ! ! !
-
     function game() {
         counter = 0;
         // while (counter<targetNumber){
@@ -26,27 +30,28 @@ $(document).ready(function () {  //wait till document loads before running
             counter = counter + parseInt($(this).data('num'));
             $('#sumOfGems').text(counter);
             winLoss();
-        }); //end game function
+            function winLoss() {
+                if (counter === targetNumber) {
+                    win++;
+                    $("#wins").html("wins: " + win);
+                    $('#sumOfGems').text("");
+                    counter = 0;
+                    alert("You Win!")
+                    values();
+                    game();
+                } else if (counter > targetNumber) {
+                    loss++;
+                    $('#sumOfGems').text("");
+                    $("#losses").html("losses" + loss);
+                    alert("You Lose!")
+                    counter = 0;
+                    values();
+                    game();
+                }
+            }
+
+        }); //end on click, game function
         console.log(counter);
+    }   //close game function
 
-    }
-    function winLoss() {
-        if (counter === targetNumber) {
-            win++;
-            $("#winLosses").html("wins" + win);
-            reset();
-        } else if (counter > targetNumber) {
-            loss++;
-            $("#winLosses").html("losses" + loss);
-            reset();
-        }
-    }
-    function reset() {
-        counter = 0;
-        targetNumber = 0;
-        setTarget();
-        setGems();
-    }
-    game();
-
-});//end document ready
+    });//end document ready
